f49abf609cce9d6603c3c5b74777e8c8a71b5c33
Diego <diegovilson.1999@gmail.com>
2018-04-26 22:31:28 -0300

    Adicionado escopo
    como usar o function corretamente

---
+++ variavel php/ESCOPO DE VARIAVEL.php
@@ -0,0 +1,21 @@
+<?php 
+$nome = 'Glaucio';
+
+//Esse é o modo padrão para criar uma função
+function teste (){
+	//usar dentro dessa função a variavel criada fora da função
+	global $nome;
+	echo $nome;
+}
+
+function teste2 (){
+	//é como se fosse outra casa, assim posso usar a mesma variavel aqui dentro
+	$nome = ' joão';
+	echo $nome.' agora no teste2';
+
+}
+
+teste ();
+teste2();
+
+ ?>
