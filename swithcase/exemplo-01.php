7c23899c1637d06ba8bf2cba3680c32b5416dfba
Diego <diegovilson.1999@gmail.com>
2018-04-29 18:56:48 -0300

    mais uma forma de repetição
    O swicth case é usado quando se tem uma afirmação e tenta chegar nela com varias afirmações

---
+++ swithcase/exemplo-01.php
@@ -0,0 +1,43 @@
+<?php 
+//Ele fala o dia na semana que se encontra no presente
+$diaDaSemana =  8;//date("w");
+//O switch funciona com uma verdade só e se não for passa para próxima e se for da break e para.
+switch ($diaDaSemana) {
+
+	case 0:
+	echo "Domingo";
+	break;
+
+	case 1:
+	echo "Sgunda-feira";
+	break;
+
+	case 2:
+	echo "Terça-feira";
+	break;
+
+	case 3:
+	echo "Quarta-feira";
+	break;
+
+	case 4:
+	echo "quinta-feira";
+	break;
+
+	case 5:
+	echo "Sexta-feira";
+	break;
+
+	case 6:
+	echo "Sabado";
+	break;
+
+	default;
+	echo "data invalida";
+}
+
+
+
+
+
+ ?>
